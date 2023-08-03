ea097cb187e7815819b66ca03ccab18e4d70f356
GuilleAQN <109323084+GuilleAQN@users.noreply.github.com>
2023-08-03 02:09:18 +0200

    Add files via upload
    Adiciones Finales pre-Documentacio

--- TicTacToe.c
+++ TicTacToe.c
@@ -6,10 +6,20 @@
 #include <windows.h>
 #include <time.h>
 
-
 char tablero[3][3];
-#define JUGADOR 'X'
-#define MAQUINA 'O'
+typedef enum
+{
+   JUGADOR_1 = 'X',
+   JUGADOR_2 = 'O',
+   MAQUINA = JUGADOR_2
+} Jugador;
+typedef enum
+{
+   JUGADOR_VS_JUGADOR = 'P',
+   JUGADOR_VS_MAQUINA = 'M'
+} ModoDeJuego;
+ModoDeJuego modoDeJuego;
+Jugador jugador;
 
 #define COLOR_NORMAL "\033[0m"
 #define COLOR_VERDE "\033[0;40;32m"
@@ -19,63 +29,69 @@ char tablero[3][3];
 void resetearTablero();
 void imprimirTablero();
 int verEspaciosLibres();
-void movimientoJugador();
+void movimientoJugador(Jugador jugadorEnTurno);
 void movimientoMaquina();
 char comprobarGanador();
-void imprimirGanador(char);
+void imprimirGanador(char ganador);
+void jugar(ModoDeJuego modoDeJuego);
+void turno(ModoDeJuego modoDeJuego);
+char seleccionarModoJuego();
+void verInstrucciones();
+void salir();
 boolean jugarDeNuevo();
+char *imprimirCaracter(char caracter);
 void limpiar();
-char* imprimirCaracter(char caracter);
+void limpiarBuffer();
 
 int main()
 {
 
-   char ganador = ' ';
-   char respuesta = ' ';
+   int opcion;
 
    do
    {
+      limpiar();
 
-      ganador = ' ';
-      respuesta = ' ';
-      resetearTablero();
+      printf("\n\t\t\t       %sTIC%s %sTAC%s %sTOE%s     ", COLOR_VERDE, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL);
+      printf("\n\t\t\t========================");
+      printf("\n\t\t\t|         MENU         |\n");
+      printf("\t\t\t========================\n");
+      printf("\t\t\t|1. Jugar              |\n");
+      printf("\t\t\t|2. Ver instrucciones  |\n");
+      printf("\t\t\t|3. Salir              |\n");
+      printf("\t\t\t========================");
+      printf("\n\t\t\t|Ingrese una opción: ");
+      scanf("%d", &opcion);
 
-      while (ganador == ' ' && verEspaciosLibres() != 0)
+      switch (opcion)
       {
-         limpiar();
-         imprimirTablero();
-
-         movimientoJugador();
-         ganador = comprobarGanador();
-         if (ganador != ' ' || verEspaciosLibres() == 0)
-         {
-            break;
-         }
-
-         limpiar();
-         movimientoMaquina();
-         ganador = comprobarGanador();
-         if (ganador != ' ' || verEspaciosLibres() == 0)
-         {
-            break;
-         }
+      case 1:
+         modoDeJuego = seleccionarModoJuego();
+         jugar(modoDeJuego);
+         break;
+      case 2:
+         verInstrucciones();
+         break;
+      case 3:
+         salir();
+         break;
+      default:
+         printf("\n\t\t\tOpción inválida. Ingrese una opción válida.");
+         Sleep(5000);
+         break;
       }
 
-      limpiar();
-      imprimirTablero();
-      imprimirGanador(ganador);
+      while (getchar() != '\n')
+         ;
 
-   } while (!jugarDeNuevo());
-
-   printf("Gracias por jugar!\n\n");
+   } while (opcion != 3);
 
    return 0;
 }
 
-// Funcion para reemplazar las 'X' o 'O' para resetear la partida
+// Función para reemplazar las 'X' o 'O' para resetear la partida
 void resetearTablero()
 {
-
    for (int i = 0; i < 3; i++)
    {
       for (int j = 0; j < 3; j++)
@@ -88,19 +104,17 @@ void resetearTablero()
 // Función para imprimir el tablero con cada valor de este
 void imprimirTablero()
 {
-
-   printf("\n\t %s | %s | %s ", imprimirCaracter(tablero[0][0]), imprimirCaracter(tablero[0][1]), imprimirCaracter(tablero[0][2]));
-   printf("\n\t---|---|---\n");
-   printf("\t %s | %s | %s ", imprimirCaracter(tablero[1][0]), imprimirCaracter(tablero[1][1]), imprimirCaracter(tablero[1][2]));
-   printf("\n\t---|---|---\n");
-   printf("\t %s | %s | %s ", imprimirCaracter(tablero[2][0]), imprimirCaracter(tablero[2][1]), imprimirCaracter(tablero[2][2]));
+   printf("\n\t\t\t  %s | %s | %s ", imprimirCaracter(tablero[0][0]), imprimirCaracter(tablero[0][1]), imprimirCaracter(tablero[0][2]));
+   printf("\n\t\t\t ---|---|---\n");
+   printf("\t\t\t  %s | %s | %s ", imprimirCaracter(tablero[1][0]), imprimirCaracter(tablero[1][1]), imprimirCaracter(tablero[1][2]));
+   printf("\n\t\t\t ---|---|---\n");
+   printf("\t\t\t  %s | %s | %s ", imprimirCaracter(tablero[2][0]), imprimirCaracter(tablero[2][1]), imprimirCaracter(tablero[2][2]));
    printf("\n\n");
 }
 
-// Función para verificar la cantidad de espacion libres para jugar
+// Función para verificar la cantidad de espacios libres para jugar
 int verEspaciosLibres()
 {
-
    int espaciosLibres = 9;
 
    for (int i = 0; i < 3; i++)
@@ -117,30 +131,43 @@ int verEspaciosLibres()
    return espaciosLibres;
 }
 
-// Función para que el jugador haga su movimiento
-void movimientoJugador()
-{
 
-   int x;
-   int y;
+// Función para que el jugador haga su movimiento
+void movimientoJugador(Jugador jugadorEnTurno)
+{
+   int x, y;
 
    do
    {
 
-      printf("Introduzca una fila #(1-3): ");
-      scanf("%d", &x);
+      printf("\t\t  Introduzca una fila #(1-3): ");
+      if (scanf("%d", &x) != 1)
+      {
+         printf("\n\t\t        Entrada inválida\n");
+         limpiarBuffer();
+         Sleep(3000);
+         break;
+      }
       x--;
-      printf("Introduzca una columna #(1-3): ");
-      scanf("%d", &y);
+      printf("\t\t  Introduzca una columna #(1-3): ");
+      if (scanf("%d", &y) != 1)
+      {
+         printf("\n\t\t        Entrada inválida\n");
+         limpiarBuffer();
+         Sleep(3000);
+         break;
+      }
       y--;
 
       if (tablero[x][y] != ' ')
       {
-         printf("Movimiento Inválido!\n");
+         printf("\n\t\t      Movimiento Inválido!\n");
+         Sleep(3000);
+         break;
       }
       else
       {
-         tablero[x][y] = JUGADOR;
+         tablero[x][y] = jugadorEnTurno;
          break;
       }
 
@@ -151,10 +178,16 @@ void movimientoJugador()
 void movimientoMaquina()
 {
 
+   printf("\t\t   Realizando movimiento");
+   for (int i = 0; i < 3; i++)
+   {
+      printf("%s.%s", COLOR_VERDE, COLOR_NORMAL);
+      Sleep(750);
+   }
+
    // Crea una semilla base en el tiempo de ahora
    srand(time(0));
-   int x;
-   int y;
+   int x, y;
 
    if (verEspaciosLibres() > 0)
    {
@@ -176,32 +209,28 @@ void movimientoMaquina()
 char comprobarGanador()
 {
 
-   // Comprobamos Filas
+   // Comprobamos Filas y Columnas
    for (int i = 0; i < 3; i++)
    {
-      if (tablero[i][0] == tablero[i][1] && tablero[i][0] == tablero[i][2])
+      bool esGanadorPorFila = tablero[i][0] == tablero[i][1] && tablero[i][0] == tablero[i][2];
+      bool esGanadorPorColumna = tablero[0][i] == tablero[1][i] && tablero[0][i] == tablero[2][i];
+
+      if (esGanadorPorFila)
       {
          return tablero[i][0];
       }
-   }
-
-   // Comprobamos Columnas
-   for (int i = 0; i < 3; i++)
-   {
-      if (tablero[0][i] == tablero[1][i] && tablero[0][i] == tablero[2][i])
+      if (esGanadorPorColumna)
       {
          return tablero[0][i];
       }
    }
 
    // Comprobamos Diagonales
-   if (tablero[0][0] == tablero[1][1] && tablero[0][0] == tablero[2][2])
+   bool esGanadorPorDiagonalPrincipal = (tablero[0][0] == tablero[1][1] && tablero[0][0] == tablero[2][2]);
+   bool esGanadorPorDiagonalSecundaria = (tablero[0][2] == tablero[1][1] && tablero[0][2] == tablero[2][0]);
+   if (esGanadorPorDiagonalPrincipal || esGanadorPorDiagonalSecundaria)
    {
-      return tablero[0][0];
-   }
-   if (tablero[0][2] == tablero[1][1] && tablero[0][2] == tablero[2][0])
-   {
-      return tablero[0][2];
+      return tablero[1][1];
    }
 
    return ' ';
@@ -210,19 +239,263 @@ char comprobarGanador()
 // Función imprimir el ganador
 void imprimirGanador(char ganador)
 {
+   if (modoDeJuego == JUGADOR_VS_JUGADOR)
+   {
+      if (ganador == JUGADOR_1)
+      {
+         printf("\t\t      GANADOR: %sJUGADOR 1%s\n", COLOR_ROJO, COLOR_NORMAL);
+      }
+      else if (ganador == JUGADOR_2)
+      {
+         printf("\t\t      GANADOR: %sJUGADOR 2%s\n", COLOR_AZUL, COLOR_NORMAL);
+      }
+      else
+      {
+         printf("\t\t           EMPATE!\n");
+      }
+   }
+   else if (modoDeJuego == JUGADOR_VS_MAQUINA)
+   {
+      if (ganador == JUGADOR_1)
+      {
+         printf("\t\t\t  GANASTE!\n");
+      }
+      else if (ganador == MAQUINA)
+      {
+         printf("\t\t\t  PERDISTE!\n");
+      }
+      else
+      {
+         printf("\t\t\t    EMPATE!\n");
+      }
+   }
+}
 
-   if (ganador == JUGADOR)
+char seleccionarModoJuego()
+{
+   char modo;
+
+   do
    {
-      printf("GANASTE!\n");
-   }
-   else if (ganador == MAQUINA)
+      fflush(stdin);
+      limpiar();
+      printf("\n\t\t\t       %sTIC%s %sTAC%s %sTOE%s     ", COLOR_VERDE, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL);
+      printf("\n\t\t\t========================");
+      printf("\n\t\t\t|         MENU         |\n");
+      printf("\t\t\t========================");
+      printf("\n\t\t\t|    Modo de juego:    |");
+      printf("\n\t\t\t|Jugador vs Jugador (%sP%s)|\n\t\t\t|Jugador vs Máquina (%sM%s)|", COLOR_VERDE, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL);
+      printf("\n\t\t\t========================");
+      printf("\n\t\t\t|Seleccione el modo de Juego: ");
+      scanf("%c", &modo);
+      modo = toupper(modo);
+      if (modo != 'P' && modo != 'M')
+      {
+         printf("\n\t\t\tPor favor, introduce una de las opciones válidas (1 o 2)");
+         Sleep(3000);
+      }
+   } while ((modo != JUGADOR_VS_JUGADOR) && (modo != JUGADOR_VS_MAQUINA));
+
+   return modo;
+}
+
+// Función para poder iniciar y jugar el Tres en Raya
+void jugar(ModoDeJuego modoDeJuego)
+{
+
+   do
    {
-      printf("PERDISTE!\n");
-   }
-   else
+
+      char ganador = ' ';
+      resetearTablero();
+
+      while (ganador == ' ' && verEspaciosLibres() != 0)
+      {
+         fflush(stdin);
+         limpiar();
+         turno(modoDeJuego);
+         imprimirTablero();
+
+         if (modoDeJuego == JUGADOR_VS_JUGADOR)
+         {
+            if (verEspaciosLibres() % 2 == 1)
+            {
+               movimientoJugador(JUGADOR_1);
+            }
+            else
+            {
+               movimientoJugador(JUGADOR_2);
+            }
+         }
+         else if (modoDeJuego == JUGADOR_VS_MAQUINA)
+         {
+            if (verEspaciosLibres() % 2 == 1)
+            {
+               movimientoJugador(JUGADOR_1);
+            }
+            else
+            {
+               movimientoMaquina();
+            }
+         }
+
+         // movimientoJugador();
+         // ganador = comprobarGanador();
+         // if (ganador != ' ' || verEspaciosLibres() == 0)
+         // {
+         //    break;
+         // }
+
+         // limpiar();
+         // movimientoMaquina();
+
+         ganador = comprobarGanador();
+         if (ganador != ' ' || verEspaciosLibres() == 0)
+         {
+            break;
+         }
+      }
+
+      limpiar();
+      imprimirTablero();
+      imprimirGanador(ganador);
+
+   } while (!jugarDeNuevo());
+}
+
+void turno(ModoDeJuego modoDeJuego)
+{
+   if (modoDeJuego == JUGADOR_VS_JUGADOR)
    {
-      printf("EMPATE!\n");
+      if (verEspaciosLibres() % 2 == 1)
+      {
+         printf("\t\t       TURNO: %sJUGADOR 1%s\n", COLOR_ROJO, COLOR_NORMAL);
+      }
+      else
+      {
+         printf("\t\t       TURNO: %sJUGADOR 2%s\n", COLOR_AZUL, COLOR_NORMAL);
+      }
    }
+   else if (modoDeJuego == JUGADOR_VS_MAQUINA)
+   {
+      if (verEspaciosLibres() % 2 == 1)
+      {
+         printf("\t\t        TURNO: %sJUGADOR%s\n", COLOR_ROJO, COLOR_NORMAL);
+      }
+      else
+      {
+         printf("\t\t       TURNO: %sMAQUINA%s\n", COLOR_AZUL, COLOR_NORMAL);
+      }
+   }
+}
+
+// Función que explica las instrucciones del juego
+void verInstrucciones()
+{
+
+   int opcionInstruccion;
+
+   do
+   {
+      limpiar();
+      printf("\n\t\t\t=============================");
+      printf("\n\t\t\t|        INSTRUCCIONES      |\n");
+      printf("\t\t\t=============================\n");
+      printf("\t\t\tTic Tac Toe es un juego cuyo \n");
+      printf("\t\t\tobjetivo es tener tu simbolo\n");
+      printf("\t\t\ten linea. Ya sea:\n");
+      Sleep(7000);
+
+      limpiar();
+      printf("\n\t\t\t=============================");
+      printf("\n\t\t\t|        INSTRUCCIONES      |\n");
+      printf("\t\t\t=============================\n");
+      printf("\t\t\t\t En vertical:");
+      printf("\n\n\t\t\t\t  %sO%s |   |  ", COLOR_AZUL, COLOR_NORMAL);
+      printf("\n\t\t\t\t ---|---|---\n");
+      printf("\t\t\t\t  %sO%s |   |  ", COLOR_AZUL, COLOR_NORMAL);
+      printf("\n\t\t\t\t ---|---|---\n");
+      printf("\t\t\t\t  %sO%s |   |  \n", COLOR_AZUL, COLOR_NORMAL);
+      Sleep(7000);
+
+      limpiar();
+      printf("\n\t\t\t=============================");
+      printf("\n\t\t\t|        INSTRUCCIONES      |\n");
+      printf("\t\t\t=============================\n");
+      printf("\t\t\t       En horizontal:");
+      printf("\n\n\t\t\t\t    |   |  ");
+      printf("\n\t\t\t\t ---|---|---\n");
+      printf("\t\t\t\t  %sX%s | %sX%s | %sX%s ", COLOR_ROJO, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL);
+      printf("\n\t\t\t\t ---|---|---\n");
+      printf("\t\t\t\t    |   |  \n");
+      Sleep(7000);
+
+      limpiar();
+      printf("\n\t\t\t=============================");
+      printf("\n\t\t\t|        INSTRUCCIONES      |\n");
+      printf("\t\t\t=============================\n");
+      printf("\t\t\t       O, en diagonal:");
+      printf("\n\n\t\t\t\t  %sO%s |   |  ", COLOR_AZUL, COLOR_NORMAL);
+      printf("\n\t\t\t\t ---|---|---\n");
+      printf("\t\t\t\t    | %sO%s |  ", COLOR_AZUL, COLOR_NORMAL);
+      printf("\n\t\t\t\t ---|---|---\n");
+      printf("\t\t\t\t    |   | %sO%s \n", COLOR_AZUL, COLOR_NORMAL);
+      Sleep(7000);
+
+      limpiar();
+      printf("\n\t\t\t=============================");
+      printf("\n\t\t\t|        INSTRUCCIONES      |\n");
+      printf("\t\t\t=============================\n");
+      printf("\n\t\t\tDeberas introducir la posición");
+      printf("\n\t\t\ten la que quieras jugar intro-");
+      printf("\n\t\t\tduciendo primero la posición ");
+      printf("\n\t\t\tde la fila y luego la columna ");
+      printf("\n\t\t\tcorrespondiente. Ej. (1,2)\n");
+      Sleep(7000);
+
+      limpiar();
+      printf("\n\t\t\t=============================");
+      printf("\n\t\t\t|        INSTRUCCIONES      |\n");
+      printf("\t\t\t=============================\n");
+      printf("\t\t\tUn ejemplo de cada posisición\n\t\t\ten el tablero sería:");
+      printf("\n\n\t\t\t     (%s1%s,%s1%s) | (%s1%s,%s2%s) | (%s1%s,%s3%s) ", COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL);
+      printf("\n\t\t\t    -------|-------|------\n");
+      printf("\t\t\t     (%s2%s,%s1%s) | (%s2%s,%s2%s) | (%s2%s,%s3%s)", COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL);
+      printf("\n\t\t\t    -------|-------|------\n");
+      printf("\t\t\t     (%s3%s,%s1%s) | (%s3%s,%s2%s) | (%s3%s,%s3%s) \n", COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL, COLOR_AZUL, COLOR_NORMAL);
+      printf("\n\t\t\t¡Buena suerte en tus partidas!");
+      Sleep(10000);
+      limpiar();
+
+      printf("\n\t\t\t=============================");
+      printf("\n\t\t\t|        INSTRUCCIONES      |\n");
+      printf("\t\t\t=============================\n");
+      printf("\n\t\t\t1. Repetir Instrucciones\n\t\t\t2. Volver al Menú");
+      printf("\n\t\t\tIngrese una opción: ");
+      scanf("%d", &opcionInstruccion);
+
+   } while (opcionInstruccion == 1 && opcionInstruccion != 2);
+
+   printf("\n\t\t\tVolviendo al Menú");
+   for (int i = 0; i < 3; i++)
+   {
+      printf("%s.%s", COLOR_VERDE, COLOR_NORMAL);
+      Sleep(750);
+   }
+}
+
+// Función que sirve para salir del juego
+void salir()
+{
+   limpiar();
+   printf("\n\n\n\t\t\t!Gracias por jugar!");
+   printf("\n\n\t\t\t    Saliendo");
+   for (int i = 0; i < 3; i++)
+   {
+      printf("%s.%s", COLOR_VERDE, COLOR_NORMAL);
+      Sleep(750);
+   }
+   printf("\n\n\n");
 }
 
 // Función para poder repetir el el juego o finalizarlo
@@ -235,7 +508,7 @@ boolean jugarDeNuevo()
 
       limpiar();
       fflush(stdin);
-      printf("Deseas jugar de nuevo (%sy%s/%sn%s): ", COLOR_AZUL, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL);
+      printf("\t\t\t¿Deseas jugar de nuevo? (%sy%s/%sn%s): ", COLOR_AZUL, COLOR_NORMAL, COLOR_ROJO, COLOR_NORMAL);
       scanf("%c", &opcion);
 
       opcion = toupper(opcion);
@@ -246,11 +519,10 @@ boolean jugarDeNuevo()
       }
       else if (opcion == 'N')
       {
-         printf("\nGracias por jugar!!!");
-         printf("\nSaliendo");
+         printf("\n\t\t\tVolviendo al Menú");
          for (int i = 0; i < 3; i++)
          {
-            printf(".");
+            printf("%s.%s", COLOR_VERDE, COLOR_NORMAL);
             Sleep(750);
          }
 
@@ -265,19 +537,28 @@ boolean jugarDeNuevo()
 }
 
 // Función para poner los colores de las X (rojo) y las X (azul)
-char* imprimirCaracter(char caracter){
-   char* color;
+char *imprimirCaracter(char caracter)
+{
+   char *color;
 
    // Determinar el formato de color según el carácter
-   if (caracter == 'X') {
+   if (caracter == 'X')
+   {
       color = COLOR_ROJO;
-   } else if (caracter == 'O') {
+   }
+   else if (caracter == 'O')
+   {
       color = COLOR_AZUL;
-   } else {
+   }
+   else
+   {
       return " "; // Retornar un espacio en blanco, en caso de que el espacio del tablero esta vació
    }
 
-   char* caracterConColor = (char*)malloc(20 * sizeof(char));
+   // Aqui se reserva memoria dinámica para almacenar una cadena de caracteres de tamaรฑo 20 y guarda el puntero a esa memoria en la variable caracterConColor
+   char *caracterConColor = (char *)malloc(20 * sizeof(char));
+
+   // La Función "sprintf" reescribe la cadena de caracteres a guardar y la almacena en el buffer de memoria dado (caracterConColor)
    sprintf(caracterConColor, "%s%c%s", color, caracter, COLOR_NORMAL);
 
    return caracterConColor;
@@ -287,4 +568,13 @@ char* imprimirCaracter(char caracter){
 void limpiar()
 {
    printf("\033[2J\033[H");
+}
+
+// Funcion la cual limpia el buffer con el fin de evitar errores
+void limpiarBuffer()
+{
+   int c; // Declaramos una variable
+   // En este bucle se toman todos los valores, se para hasta que no haya mas valores tomados por el getchar() y hasta que no sea el final del archivo (EOF) que significa que no hay mas entradas en el archivo
+   while ((c = getchar()) != '\n' && c != EOF)
+      ;
 }
